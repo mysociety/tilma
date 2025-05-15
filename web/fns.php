02671997850d257ddf0ba10e602a43840b9c9b5d
Moray Jones <moray@mysociety.org>
2025-05-15 18:28:41 +0100

    [Tilma] Download srwr data and process for streetmanger
    Download the daily srwr data and process the provided
    csv file to replicate the fields for the Streetmanager
    database and insert the data into that.
    
    https://roadworks.scot/publications/scottish-road-works-register-open-data
    
    https://github.com/mysociety/societyworks/issues/4733

--- web/fns.php
+++ web/fns.php
@@ -220,3 +220,32 @@ function db_connect() {
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $dbh;
 }
+
+function createStreetManagerQuery() {
+
+    $non_primary_columns = ['promoter_organisation', 'location',
+        'works_location_type', 'proposed_start_date', 'proposed_end_date', 'work_category',
+        'work_status', 'traffic_management_type', 'permit_status', 'close_footway'];
+    $excluded_columns = $non_primary_columns;
+    foreach ($excluded_columns as $k => $v) {
+        $excluded_columns[$k] = "EXCLUDED." . $v;
+    }
+    $dbh = db_connect();
+    $query = $dbh->prepare("INSERT INTO streetmanager
+        (permit_reference_number, " . join(', ', $non_primary_columns) . ")
+        VALUES (?" . str_repeat(",?", count($non_primary_columns)) . ")
+        ON CONFLICT (permit_reference_number) DO UPDATE SET
+        (" . join(', ', $non_primary_columns) . ") = (" . join(', ', $excluded_columns) . ")
+    ");
+
+    return $query;
+}
+
+function insertIntoStreetManager($query, $object) {
+
+    $query->execute([
+        $object['permit_reference_number'], $object['promoter_organisation'], 'SRID=27700;' . $object['works_location_coordinates'],
+        $object['works_location_type'], $object['proposed_start_date'], $object['proposed_end_date'], $object['work_category'],
+        $object['work_status'], $object['traffic_management_type'], $object['permit_status'], $object['close_footway']
+	]);
+}
